63256c4b601940801fb28c991dba1f1096451cc2
Ana Clara Matos <anasmatos28@gmail.com>
2022-06-19 23:41:31 -0300

    fixes

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -16,13 +16,10 @@ export async function getPosts(req, res) {
 export async function postPost(req, res) {
     const authorization = req.headers.authorization;
     const token = authorization.replace("Bearer", "").trim();
-    const { url, message } = req.body;
+    const { url, message, userId } = req.body;
     if (!message) message = null
     try {
-        const userId = await postsRepository.getUser(token);
-
-        const publish = await postsRepository.publishPost(url, message, userId.rows[0].userId);
-
+        const publish = await postsRepository.publishPost(url, message, userId);
         res.sendStatus(201);
     } catch (error) {
         console.log(error);
